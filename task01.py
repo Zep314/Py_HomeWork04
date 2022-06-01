e997470ae4c76e4c4e26ed769737ba5a80c1778c
Dmitry Mordanov <mordanov314@gmail.com>
2022-06-01 17:10:56 +0300

    goto home

--- task01.py
+++ task01.py
@@ -4,3 +4,22 @@
 # или [1, 7] или [1, 6, 7] и т.д. 
 # Порядок элементов менять нельзя
 
+
+def some_sequence(n,local_list): # n - с какой позиции списка начинаем искать последовательность
+    if n < 0 or n > len(local_list)-1: return [] # защита от неправильного ввода
+    ret = [local_list[n]]
+    
+    j = n
+    for i in range(n,len(local_list)):
+        if local_list[i] > local_list[j]:
+            ret.append(local_list[i])
+            j = i
+    
+    return ret
+
+my_list = [1, 5, 2, 3, 4, 6, 1, 7]
+
+print(my_list)
+print(some_sequence(0,my_list))
+print(some_sequence(6,my_list))
+
--- task02.py
+++ task02.py
@@ -1,3 +1,29 @@
 # Создать и заполнить файл случайными целыми значениями. 
 # Выполнить сортировку содержимого файла по возрастанию. 
 
+from random import randint
+
+my_filename = 'data_task02.txt'
+
+my_list = [randint(0,100) for _ in range(randint(0,100))]
+
+print(my_list)
+
+with open(my_filename,'w') as f:
+    f.write("\n".join(map(str,my_list)))
+
+my_list = []
+with open(my_filename,'r') as f:
+    my_list = f.readlines()
+    my_list = list(map(int,my_list))
+    my_list.sort()
+
+with open(my_filename,'w') as f:
+    f.write("\n".join(map(str,my_list)))
+
+    print(my_list)
+
+
+
+
+
